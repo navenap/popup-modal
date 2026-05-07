904a5a3287898791078282759b2535e636a99ae8
navenap <35478072+navenap@users.noreply.github.com>
2026-05-08 01:08:03 +0530

    Add AdminPasswordWindow for authentication

---
+++ Windows/AdminPasswordWindow.xaml.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SurveyApp
+{
+    /// <summary>
+    /// Interaction logic for AdminPasswordWindow.xaml
+    /// </summary>
+    public partial class AdminPasswordWindow : Window
+    {
+        public bool IsAuthenticated { get; private set; } = false;
+
+        public AdminPasswordWindow()
+        {
+            InitializeComponent();
+            PasswordBox.Focus();
+        }
+
+        private void ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            VisibleText.Text = PasswordBox.Password;
+            VisibleText.Visibility = Visibility.Visible;
+            PasswordBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = VisibleText.Text;
+            PasswordBox.Visibility = Visibility.Visible;
+            VisibleText.Visibility = Visibility.Collapsed;
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string password = PasswordBox.Visibility == Visibility.Visible
+                ? PasswordBox.Password
+                : VisibleText.Text;
+
+            if (password == "_+inTERnal") // change this!
+            {
+                IsAuthenticated = true;
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Invalid password");
+            }
+        }
+    }
+}
