2030c3a99d59fae572fa6f9589ea09e9c50a54c7
navenap <35478072+navenap@users.noreply.github.com>
2026-05-10 13:45:19 +0530

    Add main.swift for SurveyApp entry point

---
+++ Mac/SurveyApp/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  SurveyApp
+//
+
+import Cocoa
+
+let app = NSApplication.shared
+
+let delegate = AppDelegate()
+
+app.delegate = delegate
+
+app.setActivationPolicy(.regular)
+
+app.activate(ignoringOtherApps: true)
+
+app.run()
+
