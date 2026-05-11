4ebdbf13a0c8019b2278977115431829f1c7dd7f
navenap <35478072+navenap@users.noreply.github.com>
2026-05-11 15:06:47 +0530

    Add QuestionCache class to manage survey questions

---
+++ Windows/SurveyApp/QuestionCache.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApp
+{
+    internal class QuestionCache
+    {
+        public List<Question> Questions { get; set; } = new List<Question>();
+    }
+}
