e7c57db2726b281b49cb24bc476df7bcb2e42fa9
navenap <35478072+navenap@users.noreply.github.com>
2026-05-13 18:32:42 +0530

    Add skipCountToday and lastSkipDate to SurveyState

--- Mac/SurveyApp/SurveyState.swift
+++ Mac/SurveyApp/SurveyState.swift
@@ -2,21 +2,26 @@
 //  SurveyState.swift
 //  SurveyApp
 //
+//  Created by Meesho on 08/05/26.
+//
 
 import Foundation
 
 struct SurveyState: Codable, CustomStringConvertible {
-    var installDate: String?
     var lastAnsweredDate: String?
     var answeredToday: Bool = false
     var surveyCompleted: Bool = false
+    var skipCountToday: Int = 0
+    var lastSkipDate: String?
+
     
     var description: String {
         return """
-        installDate: \(installDate ?? "nil"),
         lastAnsweredDate: \(lastAnsweredDate ?? "nil"),
         answeredToday: \(answeredToday),
-        surveyCompleted: \(surveyCompleted)
+        surveyCompleted: \(surveyCompleted),
+        skipCountToday: \(skipCountToday),
+        lastSkipDate: \(lastSkipDate ?? "nil")
         """
     }
 }
